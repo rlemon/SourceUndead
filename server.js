0f91ca4e82f2fcfa91b682cfb8b7aa76fbba74ff
Jordan Alexander <Jordan.A.724@gmail.com>
2016-01-06 01:48:56 -0500

    Account Creation
    Rewrote account creation code for async posting
    Checks for existing account names, returns status flag true/false if the name is taken
    
    CSS changes for success alert bar

--- server.js
+++ server.js
@@ -2,16 +2,36 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-const db = require("./lib/db")
+const db = require("./lib/db");
+const bodyParser = require("body-parser");
+const post = require("./lib/posts");
 
 app.set("view engine", "ejs")
 	.use(express.static(__dirname+"/public"));
 
-app.get('/', (req, res) => res.render('login.ejs'));
-app.get('/map', (req, res) => res.render('index.ejs'));
-app.get('/create', (req, res) => res.render('createAccount.ejs'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+let loggedIn = true;
+app.get('/', (req, res) => {
+	if (!loggedIn) res.redirect("/login");
+	else res.render('index.ejs');
+});
+app.get('/login', (req, res) => res.render('login.ejs'));
+
+//account creation routing
+app.route("/create")
+	.get((req, res) => res.render('createAccount.ejs'))
+	.post((req,res) => {
+		let user = req.body.user;
+		let pass = req.body.pass;
+		let email = req.body.email;
+		post.createAccount(user,pass,email).then(response => res.send(response));
+	});
 
 const server = app.listen(8080, () => {
 	let port = server.address().port;
-	console.log(`SourceUndead has risen from the dead on port ${port}`);
+	console.log(`SourceUndead has risen from the grave on port ${port}`);
 });
