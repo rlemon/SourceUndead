5b6f3cad0e1e24657fff52e5426e16bfb54406f8
Robert Lemon <rob.lemon@gmail.com>
2016-01-20 16:41:14 -0500

    switches are lame.
    this is 100% untested :D

--- routes/io-functions.js
+++ routes/io-functions.js
@@ -1,42 +1,20 @@
 "use strict";
 import {client} from "../server";
-
+const movement = {
+	'nw':[-1,1],
+	'ne':[1,1],
+	'n':[0,1],
+	'sw':[-1,-1],
+	'se':[1,-1],
+	's':[0,-1],
+	'w':[-1,0],
+	'e':[1,0]
+};
 export function move(io, data, socket, bucket) {
-	let x = 0;
-	let y = 0; //set incrememt to player location to 0
 	if (data.direction === "null") {
 		return false;
 	}
-	switch (data.direction) {
-		case "nw":
-			x--;
-			y++;
-			break;
-		case "ne":
-			x++;
-			y++
-            break;
-		case "n":
-			y++;
-            break;
-		case "sw":
-			x--;
-			y--;
-            break;
-		case "se":
-			x++;
-			y--
-            break;
-		case "s":
-			y--;
-            break;
-		case "w":
-			x--;
-            break;
-		case "e":
-			x++;
-            break;	
-	}
+	let [x=0,y=0] = movement[data.direction];
 	if (socket.session.player.x + x > 100 || socket.session.player.x + x < 1) x = 0;
 	if (socket.session.player.y + y > 100 || socket.session.player.y + y < 1) y = 0;
 	socket.session.player.x += x;
